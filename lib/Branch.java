3c945f384d6e42d13e21c65354e168cd4476d8f0
williamfaraday123 <140964990+williamfaraday123@users.noreply.github.com>
2024-04-23 19:53:25 +0800

    Update Branch.java

--- lib/Branch.java
+++ lib/Branch.java
@@ -21,9 +21,6 @@ public class Branch implements Serializable{
 	public String getBranchName() {
 		return this.branchName;
 	}
-	public String setBranchName(string branchName){
-		this.branchName=branchName;
-	}
 	/**
 	 * 
 	 * @param branchName
