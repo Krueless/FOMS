bb35ebc86e309ccb0f5584d4406591d37d8b109d
williamfaraday123 <140964990+williamfaraday123@users.noreply.github.com>
2024-04-23 23:15:45 +0800

    Update Staff.java

--- lib/Staff.java
+++ lib/Staff.java
@@ -2,20 +2,17 @@ public class Staff extends Account implements IGetBranchName {
 
 	protected String branchName;
 	protected IDataManager orderDB;
-	protected IDisplay displayFormatter;
-	public Staff(String staffID,String password,String role,String gender,int age,String branchName,IDataManager orderDB){
+	protected IDisplayFilteredBy displayFormatter;
+	public Staff(String name,String staffID,String role,String gender,int age,String password,String branchName){
+		super.name=name;
 		super.staffID=staffID;
 		super.password=password;
 		super.role=role;
 		super.gender=gender;
 		super.age=age;
 		this.branchName=branchName;
-		if (orderDB instanceof DataManagerForOrder) {
-		    this.orderDB=orderDB;
-		} else {
-		    System.out.println("Error! Incorrect data manager");
-		}
-		this.displayFormatter=new Display();
+		this.orderDB=DataManagerForOrder.getInstance();
+		this.displayFormatter=new DisplayWithFilter();
 	}
 
 	public String getBranchName() {
@@ -26,20 +23,8 @@ public class Staff extends Account implements IGetBranchName {
 		this.branchName=branchName;
 	}
 	
-	public void displayNewOrders(IDataManager dataManager){
-		if(!(dataManager instanceof DataManagerForBranch)){
-		    return;
-		}
-		DataManagerForBranch dataManagerBranch=(DataManagerForBranch)dataManager;
-		Branch branch=dataManagerBranch.find(branchName);
-		ArrayList<Order> orderList=branch.getOrderList();
-		for(int i=0;i<orderList.size();i++){
-		    Order currentOrder=orderList.get(i);
-		    if(currentOrder.getOrderStatus()==PREPARING){
-			//displayorder
-			currentOrder.toString();
-		    }
-		}
+	public void displayNewOrders(){
+		displayFormatter.displayFilteredByBranch(orderDB.getAll(),branchName);
 	}
 	
 	public void viewOrder(int orderID){
@@ -57,8 +42,7 @@ public class Staff extends Account implements IGetBranchName {
 		Scanner sc=new Scanner(System.in);
 		switch(choice){
 		    case 1:
-		    DataManagerForBranch branchDB=DataManagerForBranch.getInstance();
-		    displayNewOrders(branchDB);
+		    displayNewOrders();
 		    break;
 		    case 2:
 		    System.out.println("Enter the orderID:");
