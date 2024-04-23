dc8b2d0ee48782903aedda8aaae75b0d4fe3680c
williamfaraday123 <140964990+williamfaraday123@users.noreply.github.com>
2024-04-23 16:30:05 +0800

    Update Admin.java

--- lib/Admin.java
+++ lib/Admin.java
@@ -2,28 +2,13 @@ public class Admin extends Account {
 	private IAdminForStaff adminForStaff;
 	private IAdminForBranch adminForBranch;
 	private IAdminForPayment adminForPayment;
-	public Admin(String name,String staffID,String role,String gender,int age,String password,IDataManager branchDB,IDataManager paymentDB,IDataManager accountDB,IDisplay displayFormatter){
+	public Admin(String name,String staffID,String role,String gender,int age,String password,IDisplay displayFormatter){
 		super.name=name;
 		super.staffID=staffID;
 		super.role=role;
 		super.gender=gender;
 		super.age=age;
 		super.password=password;
-		if(branchDB==null){
-		    System.out.println("Error: branchDB is null");
-		}else if(!(branchDB instanceof DataManagerForBranch)){
-		    System.out.println("Error: Incorrect Data Manager");
-		}
-		if(accountDB==null){
-		    System.out.println("Error: accountDB is null");
-		}else if(!(accountDB instanceof DataManagerForAccount)){
-		    System.out.println("Error: Incorrect Data Manager");
-		}
-	        if(paymentDB==null){
-	            System.out.println("Error: paymentDB is null");
-	        }else if(!(paymentDB instanceof DataManagerForPayment)){
-	            System.out.println("Error: Incorrect Data Manager");
-	        }
 	        if(displayFormatter==null){
 	            System.out.println("Error: displayFormatter is null");
 	        }
