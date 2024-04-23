3e9cf43ef6258022600087574c85ade43d4ef40a
williamfaraday123 <140964990+williamfaraday123@users.noreply.github.com>
2024-04-23 17:58:30 +0800

    Update QuotaChecker.java

--- lib/QuotaChecker.java
+++ lib/QuotaChecker.java
@@ -1,6 +1,8 @@
 public class QuotaChecker {
-  protected Admin admin;
-  protected IDataManager counter;
+  protected IDataManagerWithCount counter;
+  public QuotaChecker(){
+    this.counter=DataManagerForAccount.getInstance();
+  }
   public bool checkStaffQuota(String branchName){
     int numStaff=counter.countStaffInBranch(branchName);
     IDataManager branchDB=DataManagerForBranch.getInstance();
@@ -16,20 +18,15 @@ public class QuotaChecker {
     if(numStaff>=1 && numStaff<=4){
       if(numManager<1){
         return true;
-      }else{
-        return false;
       }
     }else if(numStaff>=5 && numStaff<=8){
-      if(numManager<1){
+      if(numManager<2){
         return true;
-      }else{
-        return false;
       }
     }else if(numStaff>=9 && numStaff<=15){
-      if(numManager<1){
+      if(numManager<3){
         return true;
-      }else{
-        return false;
       }
     }
+    return false;
 }
