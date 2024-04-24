98b4d3fb6940cb62ea816e23e20a983c09907c44
williamfaraday123 <140964990+williamfaraday123@users.noreply.github.com>
2024-04-25 03:34:28 +0800

    Update AdminForPayment.java

--- lib/AdminForPayment.java
+++ lib/AdminForPayment.java
@@ -9,21 +9,25 @@ public class AdminForPayment implements IAdminForPayment {
 	    System.out.println("Select option");
 	    System.out.println("1.Online");
 	    System.out.println("2.Credit");
-	    int choice=sc.nextInt();
-	    System.out.println("Enter name of new payment method");
-	    String name=sc.nextLine();
-	    IPayment paymentMode;
-	    if(choice==1){
-		    paymentMode=new Online(name);
-	    }else if(choice==2){
-		    paymentMode=new Credit(name);
-	    }else{
-		    System.out.println("Invalid payment option");
-		    System.out.println("Returning to user page...");
-		    sc.close();
-		    return;
+	    try{
+		    int choice=sc.nextInt();
+		    System.out.println("Enter name of new payment method");
+		    String name=sc.nextLine();
+		    IPayment paymentMode;
+		    if(choice==1){
+			    paymentMode=new Online(name);
+		    }else if(choice==2){
+			    paymentMode=new Credit(name);
+		    }else{
+			    System.out.println("Invalid payment option");
+			    System.out.println("Returning to user page...");
+			    sc.close();
+			    return;
+		    }
+		    paymentDB.add(paymentMode);
+	    }catch (Exception e){
+	            System.out.println("Choice of payment type must be number! Returning to user page...");
 	    }
-	    paymentDB.add(paymentMode);
 	    sc.close();
 	}
 	public void removePaymentMethod(){
