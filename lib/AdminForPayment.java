9566372a1d960c20a11c2b996af47cbf6d588e60
williamfaraday123 <140964990+williamfaraday123@users.noreply.github.com>
2024-04-24 11:23:00 +0800

    Update AdminForPayment.java

--- lib/AdminForPayment.java
+++ lib/AdminForPayment.java
@@ -10,11 +10,13 @@ public class AdminForPayment extends Admin implements IAdminForPayment {
 	    System.out.println("1.Online");
 	    System.out.println("2.Credit");
 	    int choice=sc.nextInt();
+	    System.out.println("Enter name of new paymentMethod");
+	    String name=sc.nextLine();
 	    IPayment paymentMode;
 	    if(choice==1){
-		    paymentMode=new Online();
+		    paymentMode=new Online(name);
 	    }else if(choice==2){
-		    paymentMode=new Credit();
+		    paymentMode=new Credit(name);
 	    }else{
 		    System.out.println("Invalid payment option");
 		    return;
