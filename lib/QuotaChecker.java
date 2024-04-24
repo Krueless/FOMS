8221df2517051c8ebd39ecaf58bbfb0fc17646a0
williamfaraday123 <140964990+williamfaraday123@users.noreply.github.com>
2024-04-24 22:41:28 +0800

    Update QuotaChecker.java

--- lib/QuotaChecker.java
+++ lib/QuotaChecker.java
@@ -1,15 +1,15 @@
 public class QuotaChecker {
     public static Boolean checkQuota(int numStaff, int numManager){
         if(numStaff >= 1 && numStaff <= 4){
-            if(numManager == 1){
+            if(numManager < 1){
                 return true;
             }
         }else if(numStaff >= 5 && numStaff <= 8){
-            if(numManager <= 2){
+            if(numManager < 2){
                 return true;
             }
         }else if(numStaff >= 9 && numStaff <= 15){
-            if(numManager <= 3){
+            if(numManager < 3){
                 return true;
             }
         }
