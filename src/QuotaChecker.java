b4769e29d28c5a700f3f19ef021960c1b39d8e79
HamsterW <shiuwesley@gmail.com>
2024-04-26 20:01:09 +0800

    updated quotaChecker

--- src/QuotaChecker.java
+++ src/QuotaChecker.java
@@ -1,13 +1,19 @@
 public class QuotaChecker {
     public static Boolean checkQuotaForAddManager(int numStaff, int numManager){
-        if (numStaff >= 1 && numStaff <= 4){
+        if (numStaff >= 0 && numStaff <= 4){
             return (numManager == 1);
         }
+        else if (numStaff == 5){
+            return (numManager == 1 || numManager == 2);
+        }
         else if (numStaff >= 5 && numStaff <= 8){
-            return (numManager <= 2);
+            return (numManager == 2);
+        }
+        else if (numStaff == 9){
+            return (numManager == 2 || numManager == 3);
         }
         else if (numStaff >= 9 && numStaff <= 15) {
-            return (numManager <= 3);
+            return (numManager == 3);
         }
         else {
             return false;
@@ -15,14 +21,20 @@ public class QuotaChecker {
     }
 
     public static Boolean checkQuotaForRemoveManager(int numStaff, int numManager){
-        if (numStaff >= 1 && numStaff <= 4){
-            return (numManager >= 1);
+        if (numStaff >= 0 && numStaff <= 4){
+            return (numManager == 1);
+        }
+        else if (numStaff == 5){
+            return (numManager == 1 || numManager == 2);
         }
         else if (numStaff >= 5 && numStaff <= 8){
-            return (numManager >= 2);
+            return (numManager == 2);
+        }
+        else if (numStaff == 9){
+            return (numManager == 2 || numManager == 3);
         }
         else if (numStaff >= 9 && numStaff <= 15) {
-            return (numManager >= 3);
+            return (numManager == 3);
         }
         else {
             return false;
@@ -31,10 +43,10 @@ public class QuotaChecker {
 
     public static Boolean checkQuotaForAddStaff(int numStaff, int numManager){
         if (numManager == 1){
-            return (numStaff <= 4);
+            return (numStaff <= 5);
         }
         else if (numManager == 2){
-            return (numStaff <= 8);
+            return (numStaff <= 9);
         }
         else if (numManager == 3){
             return (numStaff <= 15);
@@ -46,13 +58,13 @@ public class QuotaChecker {
 
     public static Boolean checkQuotaForRemoveStaff(int numStaff, int numManager){
         if (numManager == 1){
-            return (numStaff >= 1);
+            return (numStaff >= 0);
         }
         else if (numManager == 2){
-            return (numStaff >= 5);
+            return (numStaff >= 4);
         }
         else if (numManager == 3){
-            return (numStaff >= 9);
+            return (numStaff >= 8);
         }
         else{
             return false;
